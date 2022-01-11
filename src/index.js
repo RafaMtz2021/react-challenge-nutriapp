db78b98afa9505d5014a28ceba13cd8a14a27c63
LuisBalan <jlbalcz@gmail.com>
2022-01-10 20:32:46 -0600

    start routing configuration

--- src/index.js
+++ src/index.js
@@ -1,11 +1,23 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import DetailPost from "./pages/DetailPost"
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        
+        <Route path = "/" element = {<App />} />
+        <Route path="/post/:postId" element={<DetailPost />} /> 
+
+      </Routes>
+
+
+    </BrowserRouter>
+    
   </React.StrictMode>,
   document.getElementById('root')
 );
